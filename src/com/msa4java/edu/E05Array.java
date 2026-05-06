c66f6517e851d3fdd16c06577715e4770af7dccb
YEJIN-GIT <yejinaissi@gmail.com>
2026-05-06 11:24:34 +0900

    260506 init

---
+++ src/com/msa4java/edu/E05Array.java
@@ -0,0 +1,63 @@
+package com.msa4java.edu;
+
+import java.util.Arrays;
+
+public class E05Array {
+    public static void main(String[] args) {
+
+        // 배열의 복사
+    //    int[] originArr = {1, 2, 3};
+    //    int[] copyArr = originArr;
+    //
+    //    copyArr[1] = 555;
+    //    System.out.println(originArr[1]); // 555
+
+        // for문을 이용한 복사
+    //    int[] originArr = {1, 2, 3};
+    //    int[] copyArr = new int[originArr.length];
+    //    for(int 1 = 0; i < originArr.length; i++) {
+    //        copyArr[i] = originArr[i];
+    //    }
+
+        // System.arrarycopy() 이용해서 깊은 복사
+    //    int[] originArr = {1, 2, 3};
+    //    int[] copyArr = new int[originArr.length * 2];
+    //    Arrays.arraycopy(originArr, 0, copyArr, 0, originArr.length);
+    //    copyArr[1] = 55;
+    //    System.out.println(originArr[1]);   // 2
+
+        // Arrays.copyOf() 이용해서 깊은 복사
+        int[] originArr = {1, 2, 3};
+        int[] copyArr = new int[originArr.length * 2];
+        copyArr = Arrays.copyOf(originArr, originArr.length);
+        copyArr[1] = 55;
+        System.out.println(originArr[1]);   // 2
+
+        // 다차원 배열
+        int[][] arrMD = {
+                {1, 2, 3}
+                ,{4, 5, 6}
+                ,{7, 8, 9}
+        };
+        System.out.println(arrMD[2][2]);    // 9
+
+        // 배열의 API
+        int[] arrInt1 = {1, 2, 3};
+        int[] arrInt2 = {1, 2, 3};
+
+        System.out.println(arrInt1.length);
+
+        // 일차원 배열이 같은지 비교
+        System.out.println(Arrays.equals(arrInt1, arrInt2));    // true
+
+        // 다차원 배열 같은지 비교
+        int[][] arrIntDM1 = {{1, 2, 3},{4, 5, 6}};
+        int[][] arrIntDM2 = {{1, 2, 3},{4, 5, 6}};
+        System.out.println(Arrays.deepEquals(arrIntDM1, arrIntDM2));    // true
+
+        // 배열의 정렬
+        int[] arrSort = {5, 2, 76, 89, 1, 2};
+        Arrays.sort(arrSort);  // 원본이 정렬됨!
+        System.out.println(Arrays.toString(arrSort));
+    }
+}
